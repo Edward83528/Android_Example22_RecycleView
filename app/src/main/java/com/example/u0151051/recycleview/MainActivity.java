628952f2cd8079adf39a5be2e299346e569e1be6
u0151051 <u0151051@gmail.com>
2017-04-20 23:56:40 +0800

    2017/4/20

--- app/src/main/java/com/example/u0151051/recycleview/MainActivity.java
+++ app/src/main/java/com/example/u0151051/recycleview/MainActivity.java
@@ -17,8 +17,9 @@ import java.util.List;
 //Step1:RecylerView元件在「android.support.v7.widget」套件下，應用程式需要使用RecylerView元件，必須加入需要的設定。
 //Step2:開啟「Gradle Scripts -> build.gradle (Module: app)」，在「dependencies」區塊加入需要的設定： compile 'com.android.support:recyclerview-v7:23.0.+'
 //Step3:加入上列的設定後，選擇功能表「Tools -> Android -> Sync Project with Gradle Files」，讓Android Studio執行相關的設定。
-//在xml畫面區加入RecylerView,並在xml檔手動加入RecylerView的id
-//創造一個自己的 MyAdapter繼承RecyclerView.Adapter,在覆寫RecyclerView.Adapter的三個方法
+//Step4:在xml畫面區加入RecylerView,並在xml檔手動加入RecylerView的id
+//Step5:創造一個自己的 MyAdapter繼承RecyclerView.Adapter,在覆寫RecyclerView.Adapter的三個方法
+//Step6:在MyAdapter中再創一個 ViewHolder繼承 RecyclerView.ViewHolder
 public class MainActivity extends AppCompatActivity {
     RecyclerView recycleListView;
     MyAdapter myAdapter;
@@ -27,20 +28,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        try {
-            findview();
-        } catch (Exception e) {
-            Toast.makeText(MainActivity.this, e.toString(), Toast.LENGTH_LONG).show();
-        }
-    }
-
-    void findview() {
-        recycleListView = (RecyclerView) findViewById(R.id.list_view);
+        findview();
+        //建立一個ArrayList放入字串物件
         ArrayList<String> myDataset = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
             myDataset.add(Integer.toString(i));
         }
-            myAdapter = new MyAdapter(myDataset);
+        myAdapter = new MyAdapter(myDataset);
 
         //初始化 recycleListView
         //使用 LinearLayoutManager 來畫出類似 ListView 的功能。可以另外指定 GridLayouManager 或是 StaggeredGridLayoutManager 來畫出不同的外觀
@@ -48,18 +42,26 @@ public class MainActivity extends AppCompatActivity {
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
         recycleListView.setLayoutManager(linearLayoutManager);
         recycleListView.setAdapter(myAdapter);
+
+
+    }
+
+    void findview() {
+        recycleListView = (RecyclerView) findViewById(R.id.list_view);
     }
 
     //  創造一個自己的 MyAdapter繼承RecyclerView.Adapter,在覆寫RecyclerView.Adapter的三個方法
     class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         List<String> mydata;
 
+        //創造一個ViewHolder繼承 RecyclerView.ViewHolder
         class ViewHolder extends RecyclerView.ViewHolder {
             TextView tv1;
 
             public ViewHolder(View itemView) {
                 super(itemView);
-                tv1 = (TextView) findViewById(R.id.textView2);
+                //注意這裡用 itemView.findViewById
+                tv1 = (TextView) itemView.findViewById(R.id.textView);
             }
         }
 
@@ -69,15 +71,29 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout, parent, false);
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout1, parent, false);
             ViewHolder viewHolder = new ViewHolder(v);
             return viewHolder;
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
+        public void onBindViewHolder(ViewHolder holder, final int position) {
             holder.tv1.setText(mydata.get(position));
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(MainActivity.this, "你按了" + position, Toast.LENGTH_SHORT).show();
+                }
+            });
+            holder.itemView.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    Toast.makeText(MainActivity.this, "你長按了" + position, Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+            });
         }
+
 
         @Override
         public int getItemCount() {
